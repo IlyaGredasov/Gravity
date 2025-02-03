2a2f0f269621235c1e3c13377bdb8748d1506fee
IlyaGredasov <iljagredasov@yandex.ru>
2025-02-03 21:02:28 +0300

    feat: add .env support, allowance run simulation for separate user simultaneously

--- src/backend/server_main.py
+++ src/backend/server_main.py
@@ -1,27 +1,41 @@
 import os
+import json
 
 import numpy as np
-from flask import Flask, render_template, jsonify
+from dotenv import load_dotenv
+from flask import Flask, render_template, jsonify, request
 from flask_socketio import SocketIO
 from threading import Thread
 
-from space_computation import Simulation, SpaceObject
+from space_computation import Simulation, SpaceObject, CollisionType, MovementType
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+load_dotenv('.env')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 socketio = SocketIO(app, async_mode="threading")
 
-sim = Simulation([SpaceObject("First", mass=1, radius=0.01, position=np.array([1, 0]), velocity=np.array([0, 0])),
-                  SpaceObject("Second", mass=1, radius=0.01, position=np.array([-1, -1]), velocity=np.array([0, 0]))])
-calculating_thread: Thread | None = None
+users_dict: dict[str, str] = {}
+simulations_dict: dict[str, Simulation] = {}
+threads_dict: dict[str, Thread] = {}
 
 
-def simulate(simulation: Simulation):
+def simulate(user_id: str):
+    simulation = simulations_dict[user_id]
     for _ in range(int(simulation.simulation_time // simulation.time_delta)):
         simulation.calculate_step()
-        str_response = "".join(str(obj)+os.linesep for obj in simulation.space_objects)
-        socketio.emit('update_step', str_response)
-        socketio.sleep(10**-10)
+        response: json = json.dumps(
+            [{i: {"x": obj.position[0], "y": obj.position[1]}} for i, obj in enumerate(simulation.space_objects)])
+        socketio.emit('update_step', response, room=user_id)
+        socketio.sleep(0.016)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    user_id = request.sid
+    if user_id in simulations_dict.keys():
+        del simulations_dict[user_id]
+    if user_id in threads_dict.keys():
+        threads_dict[user_id].join()
+        del threads_dict[user_id]
 
 
 @app.route("/")
@@ -29,11 +43,38 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/set_simulation", methods=["POST"])
+def set_simulation():
+    data = request.json
+    try:
+        if len(list(filter(lambda x: x['movement_type'] == MovementType.CONTROLLABLE, data['space_objects']))) > 1:
+            raise ValueError("Multiple controllable objects are not supported")
+        time_delta = data.get('time_delta', Simulation.__init__.__defaults__[0])
+        simulation_time = data.get('simulation_time', Simulation.__init__.__defaults__[1])
+        G = data.get('G', Simulation.__init__.__defaults__[2])
+        collision_type = data.get('collision_type', Simulation.__init__.__defaults__[3])
+        acceleration_rate = data.get('acceleration_rate', Simulation.__init__.__defaults__[4])
+        elasticity_coefficient = data.get('elasticity_coefficient', Simulation.__init__.__defaults__[5])
+        simulation: Simulation = Simulation(space_objects=[
+            SpaceObject(name=obj['name'], mass=obj['mass'], radius=obj['radius'],
+                        position=np.array([obj['position']['x'], obj['position']['y']]),
+                        velocity=np.array([obj['velocity']['x'], obj['velocity']['y']]),
+                        movement_type=MovementType(int(obj['movement_type']))) for obj in data['space_objects']],
+            time_delta=time_delta, simulation_time=simulation_time,
+            G=G,
+            collision_type=CollisionType(int(collision_type)), acceleration_rate=acceleration_rate,
+            elasticity_coefficient=elasticity_coefficient)
+        simulations_dict[data['user_id']] = simulation
+        return jsonify({'status': 'success'}), 200
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 400
+
+
 @app.route('/launch_simulation', methods=['POST'])
 def launch_simulation():
-    global calculating_thread
-    calculating_thread = Thread(target=simulate, args=(sim,))
-    calculating_thread.start()
+    user_thread = Thread(target=simulate, args=(request.json['user_id'],))
+    user_thread.start()
     return jsonify({'status': 'started'})
 
 
--- src/backend/space_computation.py
+++ src/backend/space_computation.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 import numpy as np
 
@@ -11,66 +12,81 @@ class MovementType(Enum):
 class SpaceObject:
     def __init__(self, name: str, mass: float, radius: float, position: np.array, velocity: np.array,
                  movement_type: MovementType = MovementType.ORDINARY):
-        if len(velocity) != 2:
-            raise ValueError("Velocity must contain 2 values")
+
+        self.name: str = name
+        if mass <= 0:
+            raise ValueError("Mass must be positive")
+        self.mass: float = mass
+        if radius <= 0:
+            raise ValueError("Radius must be positive")
+        self.radius: float = radius
         if len(position) != 2:
             raise ValueError("Position must contain 2 values")
-        self.name: str = name
-        self.mass: float = mass
-        self.radius: float = radius
         self.position: np.array = position.astype(np.float64)
-        self.velocity: np.array = velocity.astype(np.float64)
+        if len(velocity) != 2:
+            raise ValueError("Velocity must contain 2 values")
+        self.velocity: np.array = np.zeros(2).astype(
+            np.float64) if movement_type == MovementType.STATIC else velocity.astype(np.float64)
         self.acceleration: np.array = np.zeros(2).astype(np.float64)
+        if movement_type not in [MovementType.STATIC, MovementType.ORDINARY, MovementType.CONTROLLABLE]:
+            raise ValueError("Invalid movement_type")
         self.movement_type: MovementType = movement_type
 
     def __repr__(self):
-        return (f"SpaceObject({self.name}, mass:{self.mass}, radius:{self.radius}, velocity:{self.velocity},"
-                f"position:{self.position}, acceleration:{self.acceleration})")
+        return (f"SpaceObject({self.name}, mass:{self.mass}, radius:{self.radius}, position:{self.position},"
+                f"velocity:{self.velocity}, acceleration:{self.acceleration}), MovementType={self.movement_type}")
 
 
 class CollisionType(Enum):
     TRAVERSING = 0
-    DESTRUCTIVE = 1
-    ELASTIC = 2
+    ELASTIC = 1
 
 
-def calculate_new_normal_velocity(first_mass, second_mass, first_velocity, second_velocity, elasticity, is_static):
-    if is_static:
-        return first_velocity
+@dataclass
+class ControllableAcceleration:
+    right: bool = False
+    left: bool = False
+    up: bool = False
+    down: bool = False
+
+
+def calculate_new_normal_velocity(first_mass, second_mass, first_velocity, second_velocity, elasticity):
     return ((first_mass - elasticity * second_mass) * first_velocity + (
-                1 + elasticity) * second_mass * second_velocity) / (first_mass + second_mass)
+            1 + elasticity) * second_mass * second_velocity) / (first_mass + second_mass)
 
 
 class Simulation:
-    GRAVITATIONAL_CONSTANT: float = 10
 
-    def __init__(self, space_objects: list[SpaceObject], time_delta: float = 10 ** -5,
-                 collision_type: CollisionType = CollisionType.ELASTIC,
-                 acceleration_rate: float = 1, elasticity_coefficient: float = 5, simulation_time: float = 10):
+    def __init__(self, space_objects: list[SpaceObject], time_delta: float = 10 ** -5, simulation_time: float = 10,
+                 G: float = 10, collision_type: CollisionType = CollisionType.ELASTIC,
+                 acceleration_rate: float = 1, elasticity_coefficient: float = 0.5):
         self.space_objects: list[SpaceObject] = space_objects
+        if time_delta <= 0:
+            raise ValueError("Time delta must be positive")
         self.time_delta: float = time_delta
-        self.collision_type: CollisionType = collision_type
-        self.acceleration_rate: float = acceleration_rate
-        self.elasticity_coefficient: float = elasticity_coefficient
+        if simulation_time <= 0:
+            raise ValueError("Simulation time must be positive")
         self.simulation_time: float = simulation_time
+        if G <= 0:
+            raise ValueError("Gravity constant must be positive")
+        self.G: float = G
+        if collision_type not in [CollisionType.TRAVERSING, CollisionType.ELASTIC]:
+            raise ValueError("Invalid collision_type")
+        self.collision_type: CollisionType = collision_type
+        if acceleration_rate <= 0:
+            raise ValueError("Acceleration rate must be positive")
+        self.acceleration_rate: float = acceleration_rate
+        if 1 < elasticity_coefficient < 0:
+            raise ValueError("Elasticity coefficient must be in [0, 1]")
+        self.elasticity_coefficient: float = elasticity_coefficient
 
     def calculate_collisions(self) -> None:
-        if self.collision_type == CollisionType.TRAVERSING:
-            return
         collisions = []
         for i in range(len(self.space_objects)):
             for j in range(i + 1, len(self.space_objects)):
                 if np.linalg.norm(self.space_objects[j].position - self.space_objects[i].position) <= \
                         self.space_objects[i].radius + self.space_objects[j].radius:
                     collisions.append((i, j))
-        if self.collision_type == CollisionType.DESTRUCTIVE:
-            destroyed = set()
-            for i, j in collisions:
-                destroyed.add(i)
-                destroyed.add(j)
-            for index in destroyed:
-                self.space_objects.pop(index)
-            return
         for i, j in collisions:
             normal_vector = (self.space_objects[j].position - self.space_objects[i].position) / np.linalg.norm(
                 self.space_objects[j].position - self.space_objects[i].position)
@@ -80,24 +96,26 @@ class Simulation:
             tangent_velocity_vector_i = np.dot(self.space_objects[i].velocity, tangent_vector)
             normal_velocity_vector_j = np.dot(self.space_objects[j].velocity, normal_vector)
             tangent_velocity_vector_j = np.dot(self.space_objects[j].velocity, tangent_vector)
-            new_normal_velocity_vector_i = calculate_new_normal_velocity(
-                self.space_objects[i].mass,
-                self.space_objects[j].mass,
-                normal_velocity_vector_i,
-                normal_velocity_vector_j,
-                self.elasticity_coefficient,
-                self.space_objects[i].movement_type == MovementType.STATIC
-            )
-
-            new_normal_velocity_vector_j = calculate_new_normal_velocity(
-                self.space_objects[j].mass,
-                self.space_objects[i].mass,
-                normal_velocity_vector_j,
-                normal_velocity_vector_i,
-                self.elasticity_coefficient,
-                self.space_objects[j].movement_type == MovementType.STATIC
-            )
-
+            if self.space_objects[i].movement_type != MovementType.STATIC:
+                new_normal_velocity_vector_i = calculate_new_normal_velocity(
+                    self.space_objects[i].mass,
+                    self.space_objects[j].mass,
+                    normal_velocity_vector_i,
+                    normal_velocity_vector_j,
+                    self.elasticity_coefficient,
+                )
+            else:
+                new_normal_velocity_vector_i = normal_velocity_vector_i
+            if self.space_objects[j].movement_type != MovementType.STATIC:
+                new_normal_velocity_vector_j = calculate_new_normal_velocity(
+                    self.space_objects[j].mass,
+                    self.space_objects[i].mass,
+                    normal_velocity_vector_j,
+                    normal_velocity_vector_i,
+                    self.elasticity_coefficient,
+                )
+            else:
+                new_normal_velocity_vector_j = normal_velocity_vector_j
             self.space_objects[
                 i].velocity = new_normal_velocity_vector_i * normal_vector + tangent_velocity_vector_i * tangent_vector
             self.space_objects[
@@ -106,13 +124,18 @@ class Simulation:
     def calculate_acceleration(self, i: int) -> np.array:
         if self.space_objects[i].movement_type == MovementType.STATIC:
             return np.zeros(2)
-        return sum(Simulation.GRAVITATIONAL_CONSTANT * self.space_objects[j].mass / np.linalg.norm(
-            self.space_objects[j].position - self.space_objects[i].position) ** 1.5 * (self.space_objects[j].position -
-                                                                                       self.space_objects[i].position)
-                   for j in range(len(self.space_objects)) if j != i)
+        return self.acceleration_rate * np.array(
+            [ControllableAcceleration.right - ControllableAcceleration.left,
+             ControllableAcceleration.up - ControllableAcceleration.down]) + sum(
+            self.G * self.space_objects[j].mass / np.linalg.norm(
+                self.space_objects[j].position - self.space_objects[i].position) ** 1.5 * (
+                    self.space_objects[j].position -
+                    self.space_objects[i].position)
+            for j in range(len(self.space_objects)) if j != i)
 
     def calculate_step(self) -> None:
-        self.calculate_collisions()
+        if self.collision_type == CollisionType.ELASTIC:
+            self.calculate_collisions()
         for i in range(len(self.space_objects)):
             if self.space_objects[i].movement_type != MovementType.STATIC:
                 self.space_objects[i].acceleration = self.calculate_acceleration(i)
